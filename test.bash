8328edd38f4dc7702ebf198b820cc57393eb2657
moritadaiki <moritadaiki@DESKTOP-KVQ8IC6.localdomain>
2022-10-27 12:23:36 +0900

    test

--- test.bash
+++ test.bash
@@ -2,4 +2,21 @@
 #SPDX-FileCopyrightText: 2022 Daiki Morita
 #SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus.py
+ng () {
+	echo NG at Line $1
+
+	res=1
+
+}
+
+res=0
+
+### I/0 test ###
+
+out=$(seq 5 | ./plus.py)
+
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
