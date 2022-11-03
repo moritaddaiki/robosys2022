ce68f7267b632e16b8f42488ebcdc420df37a0fd
moritadaiki <moritadaiki@DESKTOP-KVQ8IC6.localdomain>
2022-11-03 12:04:33 +0900

    add

--- test.bash
+++ test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash -xv
 #SPDX-FileCopyrightText: 2022 Daiki Morita
-#SPDX-License-Identifier: BSD-3-Clause
+#iSPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo NG at Line $1
@@ -13,20 +13,18 @@ res=0
 + res=0
 
 ### I/0 test ###
-
-out=$(seq 5 | ./plus.py)
+out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
 ### STRANGE INPUT ###
-
-out=$(echo あ | ./plus.py)
-["$?" = 1 ] || ng ${LINENO}
+out=$(echo あ | ./plus)
+["$?" = 1 ]      || ng ${LINENO}
 ["${out}" = "" ] || ng ${LINENO}
 
 
 
-out=$(echo | ./plus.py)
-["$?" = 1 ] || ng ${LINENO}
+out=$(echo | ./plus)
+["$?" = 1 ]      || ng ${LINENO}
 ["${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
